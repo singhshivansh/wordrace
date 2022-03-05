b64e6ef015ddae7e1a54a4fb4695db797136be03
singhshivansh <singhshivansh12@gmail.com>
2022-03-05 12:18:24 +0530

    changes

--- src/Components/GamePlay.js
+++ src/Components/GamePlay.js
@@ -59,11 +59,7 @@ const GamePlay = () => {
     }
 
     const getWord = () => {
-        // axios.get('https://random-word-api.herokuapp.com/word').
-        // then(res=>{
-        //     setword(res.data[0]);
-        // }).catch(err=>console.log(err));
-        const sentence = randomSentence({words : 5});
+        const sentence = randomSentence({words : 3});
         setword(sentence);
         
         setstackLimit(10);
@@ -132,9 +128,16 @@ const GamePlay = () => {
             </div> : null
         }
         <div className='h-full w-full'>
-            <h1 className='text-4xl font-semibold'>GamePlay</h1>
+            <div className='text-3xl mt-3 font-semibold border-x-2 border-green-500 flex justify-center'>
+                <div className='border-l-4 border-r-4 rounded-lg border-yellow-500 px-3'>
+                    GamePlay <i class="fas fa-gamepad"></i>
+                </div>
+            </div>
             <div className='flex justify-around'>
                 <h3 className='text-xl font-medium'>Player : <span className='text-green-600'>{playerName}</span></h3>
+                <div>
+                    <button className='px-4 py-1 rounded-lg bg-green-500 text-white' onClick={e=>{window.location.href = '/'}}>New Game</button>
+                </div>
             </div>
             <div className='flex justify-center'>
                 <h2 className='text-2xl font-medium bg-indigo-500 text-white my-3 px-4 py-2 rounded-xl'>{word}</h2>
@@ -144,7 +147,7 @@ const GamePlay = () => {
             <div>
                 <input className='border-2 border-green-500 rounded-lg px-3 py-2 col-6' placeholder='Enter the Sentence here' onChange={checkSpelling} value={answer} />
             </div>
-            <div className='text-xl py-3'> Misstype Left :  <span className='bg-red-500 text-white px-2 py-1 rounded-xl'>{stackLimit}</span></div>
+            <div className='text-xl py-3'> Misstype Left :  <span className='text-red-500 font-semibold rounded-xl'>{stackLimit}</span></div>
             <div className='flex justify-center'>
                 <div className='flex justify-around col-2'>
                     <button className='px-4 py-1 rounded-lg bg-red-500 text-white' onClick={resetGame}>Reset</button>
@@ -154,32 +157,71 @@ const GamePlay = () => {
             <div id="alert"></div>
 
             <div className='d-flex justify-content-center'>
-                <div className='col-6'>
-                    <table className='table'>
+                <div className='col-10 d-flex justify-content-around'>
+                    <div class="col-3 ">
+                        <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-r-4 border-l-4 border-gray-400">
+                            <div className="">
+                                {/* <div><i class="fas fa-2x fa-gamepad"></i></div> */}
+                                <p class="text-3xl font-semibold text-center text-gray-800">{score}</p>
+                                <p class="text-lg text-center text-gray-500">Score</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3 ">
+                        <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-l-4 border-r-4 border-gray-400">
+                            <div className="">
+                                {/* <div><i class="fas fa-2x fa-gamepad"></i></div> */}
+                                <p class="text-3xl font-semibold text-center text-gray-800">{level}</p>
+                                <p class="text-lg text-center text-gray-500">Level</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3 ">
+                        <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-l-4 border-r-4 border-gray-400">
+                            <div className="">
+                                {/* <div><i class="fas fa-2x fa-gamepad"></i></div> */}
+                                <p class="text-3xl font-semibold text-center text-gray-800">{multiplier}x</p>
+                                <p class="text-lg text-center text-gray-500">Multiplier</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <table className='table'>
                         <tr>
                             <th>Score</th>
                             <th>Level</th>
                             <th>Multiplier</th>
                         </tr>
-                        <tr>
-                            <td>{score}</td>
+                        <tr className='mt-2'>
+                            <td className='bg-green-500 mt-2 py-2 ' >{score}</td>
                             <td>{level}</td>
                             <td>{multiplier}</td>
                         </tr>
-                    </table>
+                    </table> */}
                 </div>
             </div>
 
+            <hr className='my-3'></hr>
+
             <div>
-                <div className='text-3xl font-semibold'>Leaderboard <i style={{color:'#FFD700'}} class="fa-solid  fa-trophy"></i> </div>
+                <div className='text-3xl font-semibold border-x-2 border-green-500 flex justify-center'>
+                    <div className='border-l-4 border-r-4 rounded-lg border-yellow-500 px-3'>
+                        Game Stats <i class="fa-solid fa-chart-pie"></i>
+                    </div>
+                </div>
 
                 <div className='flex justify-around mt-4'>
-                    <div className='col-4 flex bg-yellow-500 px-3 py-2 rounded-lg justify-between'>
-                        <table className='table p-3 col-10'>
-                            <tr>
-                                <th>Player</th>
-                                <th>Score</th>
-                            </tr>
+                    <div className='col-4 bg-yellow-600 px-3 py-2 rounded-lg justify-between'>
+                        <div className='text-white font-bold text-xl'><i style={{color:'#FFD700'}} class="fa-solid  fa-trophy"></i></div>
+                        <table className='table-auto p-3 col-10'>
+                            <thead>
+                                <tr>
+                                    <th>Player</th>
+                                    <th>Score</th>
+                                </tr>
+                            </thead>
                             {
                                 players.map(item=>{
                                    return(
@@ -193,19 +235,39 @@ const GamePlay = () => {
                         </table>
                     </div>
 
-                    <div className='col-4 flex bg-yellow-500 px-3 py-2 rounded-lg justify-between'>
-                        <table className='table p-3 col-10'>
-                            <tr>
-                                <th>Highest Score</th>
-                                <th>Average Score</th>
-                                <th>Total Games</th>
-                            </tr>
-                                <tr>
-                                    <td>{leaderboard.highestScore}</td>
-                                    <td>{leaderboard.averageScore}</td>
-                                    <td>{leaderboard.totalGame}</td>
-                                </tr>
-                        </table>
+                    <div className='col-6 flex px-3 py-2 rounded-lg justify-between'>
+                                               <div class="w-full sm:w-3/4 px-2 flex-1 my-2 transform transition delay-75 hover:scale-105">
+                            <div class="w-full ">
+                                <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-l-4 border-blue-500">
+                                    <div className="">
+                                        <p class="text-3xl font-semibold text-center text-gray-800">{leaderboard.totalGame}</p>
+                                        <p class="text-lg text-center text-gray-500">Total Games</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full sm:w-3/4 px-2 flex-1 my-2 transform transition delay-75 hover:scale-105">
+                            <div class="w-full ">
+                                <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-l-4 border-blue-500">
+                                    <div className="">
+                                        
+                                        <p class="text-3xl font-semibold text-center text-gray-800">{leaderboard.highestScore}</p>
+                                        <p class="text-lg text-center text-gray-500">Highest Score</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full sm:w-3/4 px-2 flex-1 my-2 transform transition delay-75 hover:scale-105">
+                            <div class="w-full ">
+                                <div id="jh-stats-positive" className="flex flex-col justify-center px-4 py-3 bg-white shadow-xl rounded-lg border-l-4 border-blue-500">
+                                    <div className="">
+                                        
+                                        <p class="text-3xl font-semibold text-center text-gray-800">{leaderboard.averageScore}</p>
+                                        <p class="text-lg text-center text-gray-500">Average Score</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
